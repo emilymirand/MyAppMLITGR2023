388582f556669432b66068d484adc7a96346d557
emilymirand <125322859+emilymirand@users.noreply.github.com>
2023-09-16 21:44:51 -0500

    proyecto completo

--- app/src/main/java/com/example/myappmlit2023/MainActivity.java
+++ app/src/main/java/com/example/myappmlit2023/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.myappmlit2023.ml.FlowerModel;
+import com.example.myappmlit2023.ml.ModeloFlores;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.mlkit.vision.common.InputImage;
@@ -28,10 +29,15 @@ import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
+import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.label.Category;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.List;
 
@@ -55,12 +61,13 @@ public class MainActivity
         txtResults = findViewById(R.id.txtresults);
     }
 
-    public void abrirGaleria (View view){
+    public void abrirGaleria(View view) {
         Intent i = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(i, REQUEST_GALLERY);
     }
-    public void abrirCamera (View view){
+
+    public void abrirCamera(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, REQUEST_CAMERA);
     }
@@ -97,10 +104,10 @@ public class MainActivity
     @Override
     public void onSuccess(Text text) {
         List<Text.TextBlock> blocks = text.getTextBlocks();
-        String resultados="";
+        String resultados = "";
         if (blocks.size() == 0) {
             resultados = "No hay Texto";
-        }else{
+        } else {
             for (int i = 0; i < blocks.size(); i++) {
                 List<Text.Line> lines = blocks.get(i).getLines();
                 for (int j = 0; j < lines.size(); j++) {
@@ -109,15 +116,16 @@ public class MainActivity
                         resultados = resultados + elements.get(k).getText() + " ";
                     }
                 }
-                resultados=resultados + "\n";
+                resultados = resultados + "\n";
             }
-            resultados=resultados + "\n";
+            resultados = resultados + "\n";
         }
         txtResults.setText(resultados);
     }
+
     public void Rostrosfx(View v) {
         BitmapDrawable drawable = (BitmapDrawable) mImageView.getDrawable();
-        Bitmap bitmap = drawable.getBitmap().copy(Bitmap.Config.ARGB_8888,true);
+        Bitmap bitmap = drawable.getBitmap().copy(Bitmap.Config.ARGB_8888, true);
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint();
         paint.setColor(Color.BLUE);
@@ -137,11 +145,11 @@ public class MainActivity
                     public void onSuccess(List<Face> faces) {
                         if (faces.size() == 0) {
                             txtResults.setText("No Hay rostros");
-                        }else{
+                        } else {
                             txtResults.setText("Hay " + faces.size() + " rostro(s)");
 
-                            for (Face rostro: faces) {
-                               canvas.drawRect(rostro.getBoundingBox(), paint);
+                            for (Face rostro : faces) {
+                                canvas.drawRect(rostro.getBoundingBox(), paint);
                             }
 
                         }
@@ -151,24 +159,64 @@ public class MainActivity
 
         mImageView.setImageBitmap(bitmap);
     }
+    public ByteBuffer convertirImagenATensorBuffer(Bitmap mSelectedImage){
 
-    public void PersonalizedModel(View v) {
-        try {
-            FlowerModel model = FlowerModel.newInstance(getApplicationContext());
-            TensorImage image = TensorImage.fromBitmap(mSelectedImage);
-            FlowerModel.Outputs outputs = model.process(image);
-            List<Category> probability = outputs.getProbabilityAsCategoryList();
-            Collections.sort(probability, new CategoryComparator());
-            String res="";
-            for (int i = 0; i < probability.size(); i++) {
-                res = res + probability.get(i).getLabel() + " " + probability.get(i).getScore()*100 + " % \n";
+        Bitmap imagen = Bitmap.createScaledBitmap(mSelectedImage, 224, 224, true);
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * 224 * 224 * 3);
+        byteBuffer.order(ByteOrder.nativeOrder());
+
+        int[] intValues = new int[224 * 224];
+        imagen.getPixels(intValues, 0, imagen.getWidth(), 0, 0, imagen.getWidth(), imagen.getHeight());
+
+        int pixel = 0;
+
+        for(int i = 0; i <  imagen.getHeight(); i ++){
+            for(int j = 0; j < imagen.getWidth(); j++){
+                int val = intValues[pixel++]; // RGB
+                byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
             }
-            txtResults.setText(res);
+        }
+        return  byteBuffer;
+    }
+
+    public String obtenerEtiquetayProbabilidad(String[] etiquetas, float[] probabilidades){
+
+        float valorMayor=Float.MIN_VALUE;
+        int pos=-1;
+        for (int i = 0; i < probabilidades.length; i++) {
+            if (probabilidades[i] > valorMayor) {
+                valorMayor = probabilidades[i];
+                pos = i;
+            }
+        }
+
+        return "Predicción: " + etiquetas[pos] + ", Probabilidad: " + (new DecimalFormat("0.00").format(probabilidades[pos] * 100)) + "%";
+
+    }
+
+    public void PersonalizedModel(View v){
+        try {
+
+            //Definir Estiquetas de acuerdo a su archivo "labels.txt" generado por la Plataforma de creación del Modelo
+            String[] etiquetas = {"Mario Bermúdez ", "Pedro Valencia","Derian Salazar", "Lucia Briones", "Sonia Briones ","Esperanza Agila"};
+
+            FlowerModel model = FlowerModel.newInstance(getApplicationContext());
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            inputFeature0.loadBuffer(convertirImagenATensorBuffer(mSelectedImage));
+
+            FlowerModel.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            txtResults.setText(obtenerEtiquetayProbabilidad(etiquetas, outputFeature0.getFloatArray()));
+
             model.close();
-        } catch (IOException e) {
-            txtResults.setText("Error al procesar Modelo");
+        } catch (Exception e) {
+            txtResults.setText(e.getMessage());
         }
     }
+
 
     class CategoryComparator implements java.util.Comparator<Category> {
         @Override
